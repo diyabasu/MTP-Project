fb46fb0f8d92aa14c6720f15fe89e99bf6548578
Nabati Basu <36635267+diyabasu@users.noreply.github.com>
2023-05-30 10:42:11 +0530

    Update NODEJS3.js

--- NODEJS3.js
+++ NODEJS3.js
@@ -9,4 +9,4 @@ function saveImagetodisk(url,path)
   response.pipe(localPath)})                                                   
 }
 
-saveImagetodisk("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dfree%2Bdownload&psig=AOvVaw2FiGocFA9blQ4ul1dG8ZHx&ust=1685193599735000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMD4gsGJk_8CFQAAAAAdAAAAABAE,./uploads" +Date.now()+".png")
+saveImagetodisk("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dfree%2Bdownload&psig=AOvVaw2FiGocFA9blQ4ul1dG8ZHx&ust=1685193599735000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMD4gsGJk_8CFQAAAAAdAAAAABAE",/pathIII + .png")
