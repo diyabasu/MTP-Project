a1df9a7e9c8d344c2c2b4dade1ae0b0c2ffa36e7
Nabati Basu <36635267+diyabasu@users.noreply.github.com>
2023-05-30 19:09:00 +0530

    Update NODEJS2.js

--- NODEJS2.js
+++ NODEJS2.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  // Read the PNG file
+  const imagePath = '/WORKDIR/app/imageI.png';
+  fs.readFile(imagePath, (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'imageI/png' });
+      res.end(data);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
